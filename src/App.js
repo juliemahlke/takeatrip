58b0cb128b127e0bfc9bb7d91a40b4ff7332d4ad
Julie Mahlke <mail@julie-alexandria.de>
2020-03-02 12:19:07 +0100

    Add font awesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      take a ...
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   )
 }
