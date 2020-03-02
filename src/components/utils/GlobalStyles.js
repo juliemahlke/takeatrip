58a9559a2ed74896536a782c173eaf191eeb50c4
Julie Mahlke <mail@julie-alexandria.de>
2020-03-02 12:19:25 +0100

    Add GlobalStyles

--- src/components/utils/GlobalStyles.js
+++ src/components/utils/GlobalStyles.js
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'IBM Plex Sans';
+    background: #F5F5F5;
+  }
+
   h1 {
     color: green;
     font-family: 'Fredoka One';
