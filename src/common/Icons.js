b0e908944caf297f92ea57c757667ae15858611b
Julie Mahlke <mail@julie-alexandria.de>
2020-03-14 15:00:25 +0100

    Changes after codereview

--- src/common/Icons.js
+++ src/common/Icons.js
@@ -1,6 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { far } from '@fortawesome/free-regular-svg-icons'
-//import { fal } from '@fortawesome/pro-light-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
 library.add(fas, far)
