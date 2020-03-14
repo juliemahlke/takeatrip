2ed5c530261acd14ad66e3d80b2259d146e90a87
Julie Mahlke <mail@julie-alexandria.de>
2020-03-14 12:33:29 +0100

    Add header and backlink on detailpage

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/macro'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 export default function Header() {
   return (
     <HeaderStyled>
+      <Link className="backlink" to="/">
+        <Route
+          exact
+          path="/trip/:id"
+          render={() => (
+            <FontAwesomeIcon className="icon" icon={['fas', 'chevron-left']} />
+          )}
+        />
+      </Link>
+
       <Link to="/">
         <FontAwesomeIcon className="icon" icon={['fas', 'suitcase']} />
         My Trips
       </Link>
+      <div> </div>
     </HeaderStyled>
   )
 }
 
 const HeaderStyled = styled.header`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   background: #fff;
   letter-spacing: 0.88px;
   text-align: center;
   margin: 0;
+  padding: 0 20px;
 
   a,
   a:hover {
@@ -29,6 +42,12 @@ const HeaderStyled = styled.header`
     color: #3e5a80;
     font-size: 24px;
     font-family: 'Fredoka One';
+  }
+
+  .backlink {
+    color: #3e5a80;
+    width: 20px;
+    margin-right: 0;
   }
 
   .icon {
