94313ea025cc3362645bf55f8a06f0b9795ce004
Julie Mahlke <mail@julie-alexandria.de>
2020-03-19 12:53:20 +0100

    Create a test to check if create-trip-form works

--- cypress/integration/examples/firstTest.spec.js
+++ cypress/integration/examples/firstTest.spec.js
@@ -1,6 +1,6 @@
 describe('My First Testcase', () => {
   it('Just a test!', () => {
-    cy.visit('https://localhost:3000')
+    cy.visit('http://localhost:3000')
     expect(true).to.equal(true)
   })
 })
